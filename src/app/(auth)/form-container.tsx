26a7f0d1a2d572f6508149175eef9a919736a091
Mariusz Smajdor <smajdormariusz98@gmail.com>
2023-10-17 23:44:09 +0200

    Change text content for login page in form-container component

--- src/app/(auth)/form-container.tsx
+++ src/app/(auth)/form-container.tsx
@@ -14,17 +14,17 @@ export function FormContainer({ children }: AuthContainerProps) {
 
   return (
     <main className='container relative flex h-[calc(100vh-3.5rem-1px)] items-center justify-center md:justify-normal lg:justify-evenly'>
-      <section className='z-20 flex w-full max-w-xs flex-col gap-4'>
+      <section className='z-20 flex w-full max-w-[350px] flex-col gap-4'>
         <section className='text-center'>
           <h1 className='mb-1 text-2xl font-[500]'>
             {pathname === '/register'
               ? 'Welcome to GeoGIGA'
-              : 'Welcome back to GeoGIGA'}
+              : 'Welcome Back to GeoGIGA'}
           </h1>
           <p className='text-muted-foreground'>
             {pathname === '/register'
               ? "Let's get started with your free account!"
-              : 'Login to your account!'}
+              : 'Login to play the best geographic games!'}
           </p>
         </section>
         <Button variant='outline'>
