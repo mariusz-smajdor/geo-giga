ec2740ecaa4cb674b22ca8d6a67eb4c9057ae091
Mariusz Smajdor <smajdormariusz98@gmail.com>
2023-10-17 23:42:51 +0200

    Create more restricted register zod schema

--- src/lib/zod-schema.ts
+++ src/lib/zod-schema.ts
@@ -1,12 +1,31 @@
 import { z } from 'zod';
 
+const emailSchema = z.string().trim().email('Enter valid email.');
+const loginPasswordSchema = z.string().trim();
+const registerPasswordSchema = loginPasswordSchema
+  .min(8, 'Password must be at least 8 characters long.')
+  .regex(/[a-z]/, {
+    message: 'Password must contain at least one lowercase letter.',
+  })
+  .regex(/[A-Z]/, {
+    message: 'Password must contain at least one uppercase letter.',
+  })
+  .regex(/\d/, {
+    message: 'Password must contain at least one number.',
+  })
+  .regex(/[@$!%*?&]/, {
+    message: 'Password must contain at least one special character.',
+  });
+
 export const loginSchema = z.object({
-  email: z.string().email('Enter valid email'),
-  password: z.string().min(8, 'Password must be at least 8 characters long.'),
+  email: emailSchema,
+  password: loginPasswordSchema,
 });
 
-export const registerSchema = loginSchema
-  .extend({
+export const registerSchema = z
+  .object({
+    email: emailSchema,
+    password: registerPasswordSchema,
     confirmPassword: z.string(),
   })
   .refine((data) => data.password === data.confirmPassword, {
