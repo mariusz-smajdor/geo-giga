eba586f234c63aa80c70488c24717abde5a4ac7c
Mariusz Smajdor <smajdormariusz98@gmail.com>
2023-10-19 21:03:18 +0200

    Initialize NextAuth catch-all API route using credentials provider

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,55 @@
+import NextAuth, { type NextAuthOptions } from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import { compare } from 'bcrypt';
+
+import prisma from '@/lib/prismadb';
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    CredentialsProvider({
+      name: 'credentials',
+      credentials: {
+        email: {},
+        password: {},
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          return null;
+        }
+
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.email },
+        });
+
+        if (!user || !user?.password) {
+          return null;
+        }
+
+        const passwordsMatch = await compare(
+          credentials.password,
+          user.password,
+        );
+
+        if (!passwordsMatch) {
+          return null;
+        }
+
+        return user;
+      },
+    }),
+  ],
+  pages: {
+    signIn: '/login',
+  },
+  session: {
+    strategy: 'jwt',
+  },
+  debug: process.env.NODE_ENV === 'development',
+  secret: process.env.NEXTAUTH_SECRET,
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
